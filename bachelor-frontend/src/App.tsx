927640e4e00a12bf46849e1d9c5f9c6d826a5d4c
Sergiu <sergiucraciunescu@gmail.com>
2019-11-05 11:26:44 +0100

    sidemenu/routes added

--- bachelor-frontend/src/App.tsx
+++ bachelor-frontend/src/App.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import './App.css';
-import LoginForm from './pages/LogInForm';
-import Menu from './components/Menu'
-import Header from './components/Header'
+import React, { useState } from "react";
+import "./App.css";
+import LoginForm from "./pages/LogInForm";
+import Menu from "./components/Menu";
+import Header from "./components/Header";
+import { Button } from "semantic-ui-react";
+import { MainPage } from "./pages/mainPage";
+import { Menubar } from "./components/sideMenu";
 
 const App: React.FC = () => {
+  const [sideMenuVisible, setSideMenuVisible] = useState(false);
+  const [sideMenuLocked, setSideMenuLocked] = useState(false);
+  function setSideMenuVisibleWithGuard(isVisible: boolean) {
+    if (!sideMenuLocked) {
+      setSideMenuVisible(isVisible);
+    }
+  }
+
   return (
-    <div className="Menu">
-        <LoginForm />
-        {/* <Header />
-        <Menu /> */}
+    <div>
+      <Header />
+      <div>
+        <Menubar
+          sideMenuVisible={sideMenuVisible}
+          setSideMenuVisible={setSideMenuVisibleWithGuard}
+          unlockSideMenu={() => {
+            setSideMenuLocked(false);
+            setSideMenuVisible(false);
+          }}
+          searchCallback={(input: string) => alert(input)}
+        />
+      </div>
+      {/* <LoginForm /> */}
+      {/* <Button id="button"/> */}
+      {/* <SideMenu/> */}
+      {/* <MainPage /> */}
     </div>
   );
-}
+};
 
 export default App;
--- bachelor-frontend/src/pages/mainPage.tsx
+++ bachelor-frontend/src/pages/mainPage.tsx
@@ -1,11 +1,26 @@
-import React, {useEffect,useState} from "react"
+import React, { useEffect, useState } from "react";
+import { Grid } from "semantic-ui-react";
+import { Path } from "../lib/definitions/enums";
+import { Switch, Route } from "react-router";
+import rentalOverview from "./rentalOverview";
 
-
-export const MainPage: React.FC = () =>{
-
-    return(
-        <React.Fragment>
-        </React.Fragment>
-    )
-
-}
+export const MainPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Switch>
+        <Route exact path={Path.RentalOverview} component={rentalOverview}/>
+      
+      <Grid>
+        <Grid.Row>
+          <Grid.Column style={{ backgroundColor: "red" }} width={3}>
+            {/* <SideMenu /> */}
+          </Grid.Column>
+          <Grid.Column style={{ backgroundColor: "aqua" }} width={13}>
+            <h1>Second one starts from here</h1>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      </Switch>
+    </React.Fragment>
+  );
+};
